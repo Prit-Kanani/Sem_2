afeedef005e5db8dca5c88f782afadd2565d2ce7
Prit-Kanani <146157602+Prit-Kanani@users.noreply.github.com>
2024-01-31 21:30:14 +0530

    Delete lab 8 directory

--- lab 8/8A1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main () {
-	float a,i=0;
-	while(i<=9)
-	{
-		printf("%f\n",sqrt(i));
-		i++;
-	}
-}
--- lab 8/8A2a.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main() {
-	int i=1,n=10;
-	while(i<=10)
-	{
-		printf("%d",i);
-		i=i+2;
-	}
-}
