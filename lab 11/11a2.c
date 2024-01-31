eadfcd5e2b786ee924ed2d9d069a2b65c243e415
Prit-Kanani <146157602+Prit-Kanani@users.noreply.github.com>
2024-01-31 21:28:38 +0530

    Delete lab 11 directory

--- lab 11/11a2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main(){
-	int a,i=1,sum=0;
-	scanf("%d",&a);
-	for(i=1;i<=a;i++){
-		sum=sum+i;
-	}
-	printf("%d",sum);
-}
--- lab 11/11a3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main(){
-	int a,i=1;
-	scanf("%d",&a);
-	for(i=1;i<=10;i++){
-		printf("%d x %d = %d\n",a,i,a*i );
-	}
-	
-}
